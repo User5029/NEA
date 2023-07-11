d6b20a783c12e5cea84f2f0fe432c56cd96eb18c
User5029 <69436519+User5029@users.noreply.github.com>
2023-07-11 19:37:41 +0100

    Packet manager + audio
    Added nuget package manager and install websocket-sharp-netstandard. Also added the audio library

--- control_server/index.js
+++ control_server/index.js
@@ -5,19 +5,23 @@ const wss = new WebSocket.Server({ port: 8080 }, () => {
 
 let counter = 0
 wss.on('connection', function connection(ws) {
-   
+   console.log("Client Connected.")   
    ws.on('message', (data, isBinary) => {
       const message = isBinary ? data : data.toString();
       console.log('data received \n %o', message);
       if(counter === 0){
-         ws.send('c:/music/7.mp3');
+         ws.send('c:/music/test2.mp3');
          counter = counter + 1
       } else if(counter === 1){
-         ws.send('c:/music/13. Cheerleading.mp3')
+         ws.send('c:/music/test.mp3')
          counter = 0
       }
    })
 })
 wss.on('listening', () => {
    console.log('listening on 8080');
+})
+
+wss.on('close', (data) => {
+   console.log('Client disconnected.')
 })
