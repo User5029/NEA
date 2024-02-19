024cfaa7150a3f763e71c8e5f66700c1d020c0b6
User5029 <69436519+User5029@users.noreply.github.com>
2024-02-19 13:22:56 +0000

    fixed sql query

--- control_server2/src/functions/data.sql
+++ control_server2/src/functions/data.sql
@@ -1,36 +1,54 @@
-CREATE TABLE IF NOT EXISTS show (
-    _id varchar(255) PRIMARY KEY AUTOINCREMENT,
-    show_name varchar(255) NOT NULL
+CREATE TABLE IF NOT EXISTS `show` (
+	`_id` INT(100) NOT NULL AUTO_INCREMENT,
+	`show_name` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`_id`) USING BTREE
 )
 
-CREATE TABLE IF NOT EXISTS cue_table (
-    _id varchar(255) PRIMARY KEY AUTOINCREMENT,
-    show_id varchar(255) FOREIGN KEY REFERENCES show(_id),
-    cue_number int(255),
-    audio_cue varchar(255) FOREIGN KEY REFERENCES audio_table(_id),
-    midi_cue varchar(255) FOREIGN KEY REFERENCES,
-    notes varchar(255) FOREIGN KEY REFERENCES ,
-    
+CREATE TABLE IF NOT EXISTS `audio` (
+	`_id` INT(100) NOT NULL AUTO_INCREMENT,
+	`show_id` INT(100) NOT NULL,
+	`filePath` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`preWait` FLOAT NOT NULL DEFAULT '0',
+	`fadeIn` FLOAT NOT NULL DEFAULT '0',
+	`fadeOut` FLOAT NOT NULL DEFAULT '0',
+	`postWait` FLOAT NOT NULL DEFAULT '0',
+	`volume` FLOAT NOT NULL DEFAULT '0.9',
+	PRIMARY KEY (`_id`) USING BTREE,
+	INDEX `show_id` (`show_id`) USING BTREE,
+	CONSTRAINT `show_id` FOREIGN KEY (`show_id`) REFERENCES `show` (`_id`) ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 
-CREATE TABLE IF NOT EXISTS audio_table (
-    _id varchar(255) PRIMARY KEY AUTOINCREMENT,
-    show_id varchar(255) FOREIGN KEY REFERENCES show(_id),
-    file_path varchar(255) NOT NULL,
-    prewait float(16) DEFAULT 0,
-    fadeIn float(16) DEFAULT 0,
-    fadeOut float(16) DEFAULT 0,
-    postWait float(16) DEFAULT 0,
-    volume float(8) DEFAULT 0.9
+CREATE TABLE IF NOT EXISTS `midi` (
+	`_id` INT(100) NOT NULL AUTO_INCREMENT,
+	`show_id` INT(100) NOT NULL,
+	PRIMARY KEY (`_id`) USING BTREE,
+	INDEX `showid` (`show_id`) USING BTREE,
+	CONSTRAINT `showid` FOREIGN KEY (`show_id`) REFERENCES `show` (`_id`) ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 
-CREATE TABLE IF NOT EXISTS midi_cue (
-    _id varchar(255) PRIMARY KEY AUTOINCREMENT,
-    show_id varchar(255) FOREIGN KEY REFERENCES show(_id),
+CREATE TABLE IF NOT EXISTS `notes` (
+	`_id` INT(100) NOT NULL AUTO_INCREMENT,
+	`show_id` INT(100) NOT NULL,
+	`data` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`_id`) USING BTREE,
+	INDEX `showid` (`show_id`) USING BTREE,
+	CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`show_id`) REFERENCES `show` (`_id`) ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 
-CREATE TABLE IF NOT EXISTS notes (
-    _id varchar(255) PRIMARY KEY AUTOINCREMENT,
-    show_id varchar(255) FOREIGN KEY REFERENCES show(_id),
-    note TEXT(65535)
+CREATE TABLE IF NOT EXISTS `cue` (
+	`_id` INT(100) NOT NULL AUTO_INCREMENT,
+	`show_id` INT(100) NOT NULL,
+	`cueNumber` INT(100) NOT NULL,
+	`audioCue` INT(100) NOT NULL,
+	`midiCue` INT(100) NOT NULL,
+	`noteCue` INT(100) NOT NULL,
+	PRIMARY KEY (`_id`) USING BTREE,
+	INDEX `showid2` (`show_id`) USING BTREE,
+	INDEX `audioid` (`audioCue`) USING BTREE,
+	INDEX `midicue` (`midiCue`) USING BTREE,
+	INDEX `notecue` (`noteCue`) USING BTREE,
+	CONSTRAINT `audioid` FOREIGN KEY (`audioCue`) REFERENCES `audio` (`_id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `midicue` FOREIGN KEY (`midiCue`) REFERENCES `midi` (`_id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `notecue` FOREIGN KEY (`noteCue`) REFERENCES `notes` (`_id`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `showid2` FOREIGN KEY (`show_id`) REFERENCES `show` (`_id`) ON UPDATE NO ACTION ON DELETE NO ACTION
 )
