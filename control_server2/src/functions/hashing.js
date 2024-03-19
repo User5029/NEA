853e0be4326903e401b5ce9dcf5ac30e678c0154
User5029 <69436519+User5029@users.noreply.github.com>
2024-03-19 11:16:56 +0000

    start of hashing algorithm

---
+++ control_server2/src/functions/hashing.js
@@ -0,0 +1,39 @@
+
+
+class HASHING {
+    constructor() {
+
+    }
+
+
+    async hash(string) {
+        let sum = 0
+        let tot = 0
+
+        for(const i of string){
+            let char = i.charCodeAt(0)
+            sum = sum + char
+            tot += 1 + 0.01*char
+
+            if(sum === Infinity){
+                sum = sum - char
+                sum = Math.floor(sum/3)
+            }
+            if(tot === Infinity){
+                tot = tot - char
+                tot = Math.floor(tot/356)
+            }
+        }
+
+        let num = Math.sqrt(sum)*tot*Math.PI
+        if(num === Infinity){
+            num = Math.sqrt(sum)*Math.sqrt(tot)*0.5
+        }
+        let slot = num % 98765234
+        slot = Math.floor(slot)
+
+        return slot
+    }
+}
+
+module.exports = {HASHING}
--- control_server2/src/index.js
+++ control_server2/src/index.js
@@ -4,15 +4,27 @@ Imports all the managers and client needed to run the program
 const {DB} = require('./functions/database')
 const {CLIENT} = require('./functions/client')
 const {WEBSOCKET} = require('./functions/websocket')
+const {HASHING} = require('./functions/hashing')
 
 /*
 Sets up the clients by passing various variables that may be required
 */
 
+const hash = new HASHING()
 const client = new CLIENT()
 client.config = require('./config.json')
-const database = new DB(client)
-const WebSocket = new WEBSOCKET(client, database)
+//const database = new DB(client)
+//const WebSocket = new WEBSOCKET(client, database)
+
+
+async function test(){
+  let reply = await hash.hash("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+  console.log(reply)
+}
+
+test()
+
+
 
 /**
  * The rest of the code for the application goes here
@@ -21,9 +33,9 @@ const WebSocket = new WEBSOCKET(client, database)
 
 // Function done when the program initially starts
 async function start() {
-  await database.connect() // 
+  //await database.connect() // 
 }
 
 start()
 
-database.DBQuery(`INSERT INTO NEA.show (show_name) VALUES (?);`, ");DROP TABLE show;--")
+//database.DBQuery(`INSERT INTO NEA.show (show_name) VALUES (?);`, ");DROP TABLE show;--")
