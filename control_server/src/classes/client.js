b94b00ef805af3e9dd206ca6f179818b5e7ddd64
User5029 <james@haswelluk.com>
2025-01-13 08:18:58 +0000

    Update client.js

--- control_server/src/classes/client.js
+++ control_server/src/classes/client.js
@@ -1,6 +1,8 @@
 const {QUEUE} = require('./queue')
  
-
+/*
+This class inherits all the properties from the Queue class by using the ‘extends’ keyword. This is saying the CLIENT class is an extension of the QUEUE class.
+*/
 class CLIENT extends QUEUE{
     constructor() {
       super()
